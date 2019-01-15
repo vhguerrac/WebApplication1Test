cf400918f1dc5de1170c926a56915828bd1a128c
Vic <vhguerrac@hotmail.com>
2019-01-14 23:41:43 -0500

    Primer Commit Test

---
+++ WebApplication1/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ContractTypeName { get; set; }
+        public int RoleId { get; set; }
+        public string RoleName { get; set; }
+        public string RoleDescription { get; set; }
+        public decimal HourSalary { get; set; }
+        public decimal MonthlySalry { get; set; }
+    }
+}
---
+++ business/business.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data;
+
+namespace Business
+{
+    public class FBusiness
+    {
+        public IEnumerable<object> GetEmployees()
+        {
+            Employees employees = new Employees();
+            return employees as IEnumerable<object>;
+        }
+    }
+}
